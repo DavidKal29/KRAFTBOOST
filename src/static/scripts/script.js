ae4ede14f7b64ff7762e767bc415405ba717c5c6
DavidKal29 <davidkalmuk65@gmail.com>
2025-04-29 10:52:14 +0200

    Añadimos en static la carpeta scripts, y metemos el script que controla el menu hamburguesa en script.js. Enlazamos al final del body el script

---
+++ src/static/scripts/script.js
@@ -0,0 +1,18 @@
+//Menú Hamburguesa Lógica
+
+//Obtenemos el boton para abrir o cerrar
+document.getElementById('boton_hamburguesa').addEventListener('click', function() { 
+    
+    //Obtenemos el div que es el menu hamburguesa
+    let menu=document.getElementById('menu_hamburguesa'); 
+    
+    //Vemos si el menu está escondido o no, y dependiendo de eso, lo escondemos o lo hacemos aparecer
+    menu.style.right = menu.style.right==='0px'?'-100%':'0px';
+
+    //Obtenemos el icono del boton
+    let icono_boton=document.getElementById('icono_boton') 
+
+    //Esto hace que si la classlist es una de esas, cambia a la otra
+    icono_boton.classList.toggle("fa-bars"); 
+    icono_boton.classList.toggle("fa-x");       
+});
