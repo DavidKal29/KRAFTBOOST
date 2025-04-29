b66f97012d85cf97466efc00f6ca5194752e6db6
DavidKal29 <davidkalmuk65@gmail.com>
2025-04-29 13:08:26 +0200

    Añadimos la conexión a la base de datos

--- src/app.py
+++ src/app.py
@@ -2,11 +2,17 @@ from flask import Flask
 from os import getenv
 from config import config
 from dotenv import load_dotenv
+from flask_mysqldb import MySQL
 
 load_dotenv()
 
 app=Flask(__name__)
+
+#Establecemos la configuración de la app
 app.config.from_object(config['development'])
+
+#Conexión de la base de datos
+db=MySQL(app)
 
 
 if __name__=='__main':
