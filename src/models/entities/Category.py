2a569c636b388ccb00b43b65273fce2567d64165
DavidKal29 <davidkalmuk65@gmail.com>
2025-04-30 11:09:53 +0200

    Creamos los objetos correspondientes en Category.py y Product.py

---
+++ src/models/entities/Category.py
@@ -0,0 +1,11 @@
+class Category():
+
+    def __init__(self,id,nombre,imagen):
+        self.id=id
+        self.nombre=nombre
+        self.imagen=imagen
+        
+
+    
+
+
---
+++ src/models/entities/Product.py
@@ -0,0 +1,10 @@
+class Product():
+
+    def __init__(self,id,nombre, precio, id_categoria, id_marca,descripcion,imagen):
+        self.id=id
+        self.nombre=nombre
+        self.precio=precio
+        self.id_categoria=id_categoria
+        self.id_marca=id_marca
+        self.descripcion=descripcion
+        self.imagen=imagen
