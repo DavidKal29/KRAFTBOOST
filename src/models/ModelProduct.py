8a9240e30eb95f666bbbb666eac1512b4415caa3
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-01 13:09:25 +0200

    Creamos la funcion mostrar_productos_paginacion en ModelProduct que recibirá el page de /shop y los productos que se mostrarán por pagina y a partir de ahi se hará la consulta a la db. Tambien en shop.py, desarrollamos la logica de la paginacion y como mostrar los productos, evitando que se ponga 0 o un numero mayor al permitido en el page, y redirijiendo a pege=1 si se va a /shop sin parametros

--- src/models/ModelProduct.py
+++ src/models/ModelProduct.py
@@ -6,7 +6,6 @@ class ModelProduct():
     @classmethod
     def mostrar_productos(cls,db,order):
         try:
-            print(1)
             #Se abre un cursor con la conexion a la db y se crea la consulta sql
             cursor=db.connection.cursor()
 
@@ -54,3 +53,64 @@ class ModelProduct():
         except Exception as error:
             print(error)
             return None
+        
+
+    
+
+    #Metodo para /shop, donde se mostraran los productos y se filtraran con LIMIT y OFFSET
+    # para que se muestren asi con la paginacion
+    @classmethod
+    def mostrar_productos_paginacion(cls,db,page,productos_por_pagina):
+        try:
+            #Se abre un cursor con la conexion a la db
+            cursor=db.connection.cursor()
+
+            #El limit seran el numero de productos que apareceran
+            # en este caso, los productos por pagina
+            limit=productos_por_pagina
+
+            #El offset es cuantos productos se saltará, pero claro, si page es 1, 
+            # debe saltarse 0, por tanto restamos 1 a page
+            offset=(page-1)*productos_por_pagina
+
+            print('El limit:',limit, 'y el Offset:',offset)
+            
+            #Creamos la consulta con sus values
+            sql='SELECT id,nombre,precio,imagen FROM productos ORDER BY id DESC LIMIT %s OFFSET %s'
+            
+            values=(limit,offset)
+            print('Los valores=',values)
+
+            #Ejecutamos la consulta
+            cursor.execute(sql,values)
+            resultados=cursor.fetchall()
+           
+
+            #Si la consulta devuelve datos, creamos una lista, recorremos los datos 
+            # y creamos un objeto con cada producto, metiendolos en la lista
+            if resultados:
+                productos=[]
+
+                for resultado in resultados:
+                    id=resultado[0]
+                    nombre=resultado[1]
+                    precio=resultado[2]
+                    imagen=resultado[3]
+
+                    productos.append(Product(id,nombre, precio, None, None, None, imagen))
+
+                cursor.close()
+                return productos
+                
+            #Si no hay resultados, retornamos None    
+            else:
+                cursor.close()
+                return None
+        
+        
+        #Si hay errores, devolvemos None tambien
+        except Exception as error:
+            print(error)
+            return None
+
+    
--- src/routes/shop.py
+++ src/routes/shop.py
@@ -1,9 +1,54 @@
-from flask import Blueprint,render_template,current_app
+from flask import Blueprint,redirect,request,url_for,current_app,render_template
+from models.ModelProduct import ModelProduct
+import math
 
 shop_bp=Blueprint('shop',__name__)
 
+#Al redirijir a shop, usaré shop.shop que es basicamente 
+# el name del bluerpint y el nombre de la funcion de la ruta
 @shop_bp.route('/shop')
-def home():
+def shop():
+
+    #Si no hay page, redirijimos a page 1
+    if not request.args.get('page'):
+        return redirect(url_for('shop.shop', page=1))
+    
+    #Obtenemos el db de la app
+    db=current_app.config['db']
+
+    #Obtenemos el numero de productos
+    try:
+        
+        cursor=db.connection.cursor()
+        cursor.execute('SELECT COUNT(*) FROM productos')
+        
+        total=cursor.fetchone()
+        total=total[0]
+        
+        cursor.close()
+    except Exception as error:
+        print(error)
 
 
-    return 'Esta es la ruta de la tienda'
+    #Definimos el numero de paginas
+    productos_por_pagina=12
+    
+    #Redondeo al maximo por si hay decimales
+    pagina_maxima=math.ceil(total/productos_por_pagina)
+
+    #Obtengo el page
+    page=int(request.args['page'])
+
+    #Si se pasa, lo lleva al maximo permitido
+    if page>pagina_maxima:
+        return redirect(url_for('shop.shop', page=pagina_maxima))
+    
+    #Si no llega, lo lleva al minimo
+    elif page<1:
+        return redirect(url_for('shop.shop', page=1))
+    
+    else:
+        #Obtenemos los productos con los filtros de la paginacion
+        productos=ModelProduct.mostrar_productos_paginacion(db,page,productos_por_pagina)
+
+        return render_template('shop.html')
