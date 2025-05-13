3d7c581aaacd04dee789099d75b1481bec374dd1
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-13 15:21:39 +0200

    Hacemos que si alguien intenta acceder a /auth a secas y no a /auth/login o /register, que lo redirija directamente a /auth/login para evitar errores o problemas a futuro.

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint,redirect,url_for
 
 auth_bp=Blueprint('auth',__name__,url_prefix='/auth')
 
+
+@auth_bp.route('/',methods=['GET','POST'])
+def auth():
+    return redirect(url_for('auth.login'))
 
 @auth_bp.route('/login',methods=['GET','POST'])
 def login():
