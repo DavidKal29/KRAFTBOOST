15434084fa55e0f945ac5af0864e3e2e39f02361
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-13 09:23:19 +0200

    Utilizamos abort de flask, para que, en caso de que a través de la ruta intentemos entrar a ver un producto cuyo id no existe en la base de datos.

--- src/routes/page_product.py
+++ src/routes/page_product.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,redirect,request,url_for,current_app,render_template
+from flask import Blueprint,redirect,request,url_for,current_app,render_template,abort
 from models.ModelProduct import ModelProduct
 
 
@@ -12,4 +12,9 @@ def product(id):
 
     producto=ModelProduct.mostrar_producto_info(db,id)
 
-    return render_template('product.html',id=id,producto=producto)
+    if not producto:
+        abort(404)
+    
+    else:
+
+        return render_template('product.html',id=id,producto=producto)
