3605dc3aab3c36cd58518e80ada79ec97f51e16b
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-15 10:41:06 +0200

    Añadimos en la carpeta entities el User.py, que será el objeto que contendrá a los usuarios.

---
+++ src/models/entities/User.py
@@ -0,0 +1,23 @@
+from werkzeug.security import check_password_hash
+from flask_login import UserMixin
+
+class User(UserMixin):
+
+    def __init__(self,id,nombre,apellidos,email,username,telefono,fecha_nacimiento,password,fecha_registro,rol):
+        self.id=id
+        self.nombre=nombre
+        self.apellidos=apellidos
+        self.email=email
+        self.username=username
+        self.telefono=telefono
+        self.fecha_nacimiento=fecha_nacimiento
+        self.password=password
+        self.fecha_registro=fecha_registro
+        self.rol=rol
+
+    @classmethod
+    def checkPassword(cls,hashed_password,password):
+        return check_password_hash(hashed_password,password)
+    
+
+
