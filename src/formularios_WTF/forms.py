4a144b23a88a44cebcee0eaaa520a326a99bdbab
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-16 13:55:25 +0200

    Añadimos una funcion para validar constraseñas, las cuales deberan tener 8 caracteres y minimo 1 letra mayuscula, 1 letra minuscula y 1 numero, por lo menos.

--- src/formularios_WTF/forms.py
+++ src/formularios_WTF/forms.py
@@ -1,6 +1,20 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField,EmailField,PasswordField,SubmitField
-from wtforms.validators import DataRequired,Length,EqualTo,Email
+from wtforms.validators import DataRequired,Length,EqualTo,Email,ValidationError
+import re
+
+
+def validar_password(form, field):
+
+    #Validamos que tenga al menos 1 minuscula, 1 mayuscula, 1 numero
+    if not re.search(r'\d', field.data):
+        raise ValidationError('Al menos 1 digito en la contraseña')
+
+    if not re.search(r'[A-Z]', field.data):
+        raise ValidationError('Al menos 1 mayúscula en la contraseña')
+
+    if not re.search(r'[a-z]', field.data):
+        raise ValidationError('Al menos 1 minúscula en la contraseña')
 
     
 
@@ -31,6 +45,7 @@ class Register(FlaskForm):
     password=PasswordField('Contraseña',validators=[
         DataRequired(message="Este campo es obligatorio"),
         Length(min=8,max=100,message='8-100 caracteres requeridos'),
+        validar_password
     ])
 
     confirm=PasswordField('Confirmar Contraseña',validators=[
