7929f187e7c296d9e245b4563c0702bc88f46601
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-17 17:25:10 +0200

    Instalamos la libreria Flask-Mail que se usará para envio de emails automáticos y recuperar contraseña. Ponemos las configuraciones necesarias en config.py, y creamos el archivo MailSender.py en utils para realizar los distintos tipos de envio de correos.

--- src/app.py
+++ src/app.py
@@ -1,5 +1,6 @@
 from flask import Flask,render_template
 from flask_login import LoginManager
+from flask_mail import Mail
 from os import getenv
 from config import config
 from dotenv import load_dotenv
@@ -29,6 +30,9 @@ app.config.from_object(config['development'])
 
 #Conexión de la base de datos
 db=MySQL(app)
+
+#Conexión al email
+mail=Mail(app)
 
 #Añadimos la db al config de app
 app.config['db']=db
--- src/config.py
+++ src/config.py
@@ -6,6 +6,15 @@ load_dotenv()
 
 class Config():
     SECRET_KEY=os.getenv('SECRET_KEY')
+    
+    #Enviar Correos
+    MAIL_SERVER='smtp.gmail.com'
+    MAIL_PORT=587
+    MAIL_USE_TLS=True
+    MAIL_USERNAME=os.getenv('CORREO')
+    MAIL_PASSWORD=os.getenv('PASSWORD_DEL_CORREO')
+
+
 
 class Development(Config):
     DEBUG=True
