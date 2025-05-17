f1a7e2567f51098611de8d24345c3f959b8a14d9
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-17 22:17:51 +0200

    Creamos el metodo en MailSender para enviar el email que te lleve al formulario para reestablecer la contraseña.

--- src/utils/MailSender.py
+++ src/utils/MailSender.py
@@ -28,3 +28,27 @@ class MailSender:
 
         #El correo se envia
         current_app.config['mail'].send(Mensaje)
+
+    @classmethod
+    def reset_password_message(cls,current_app,email,ip,token):
+
+        #El asunto
+        asunto='Recuperación Contraseña'
+
+        #Mensaje de bienvenida
+        html='''
+            <p>Para recuperar la contraseña, accede al enlace de abajo</p>
+            <a href="{}auth/reset_password/{}">Reestablecer Contraseña</a>
+        '''.format(ip,token)
+
+        #Estructura básica para enviar el mensaje, usando el current 
+        # app que traeré de las rutas, porque no puedo usarlo fuera de las rutas
+        Mensaje=Message(subject=asunto,
+              sender=current_app.config['MAIL_USERNAME'],
+              recipients=[email])
+
+        #Añadimos el html al mensaje
+        Mensaje.html=html
+
+        #El correo se envia
+        current_app.config['mail'].send(Mensaje)
