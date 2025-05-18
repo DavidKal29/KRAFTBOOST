c30196df182a634d0d731eac3dcda326db987226
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-18 21:09:51 +0200

    Acutalizamos el metodo addProductCart para ver si el producto ya fue añadido antes o es la primera vez que se añade al carrito y se actua en base a ello. Tambien se crean los metodos removeOneProductCart y removerProductCart para eliminar quitar uno a la cantidad de un producto en el carrito o removerlo totalmente del carrito el producto.

--- src/services/CartService.py
+++ src/services/CartService.py
@@ -1,7 +1,7 @@
 class CartService:
 
     @classmethod
-    def addProduct(cls,db,id_usuario,id_producto):
+    def addProductCart(cls,db,id_usuario,id_producto):
         try:
             #Se abre el cursor de la db
             cursor=db.connection.cursor()
@@ -22,17 +22,34 @@ class CartService:
                     print('Sin stock')
                     return None
                 
-                #Insertamos en la tabla carrito, el id del usuario,producto,cantidad del producto y precio total
-                sql='INSERT INTO carrito (id_usuario,id_producto,cantidad,precio) VALUES (%s,%s,%s,%s)'
-                cursor.execute(sql,(id_usuario,id_producto,1,precio))
-                db.connection.commit()
+                #Consultamos a ver si el producto ya fue añadido al carrito antes
+                sql='SELECT cantidad FROM carrito WHERE id_usuario=%s and id_producto=%s'
+                cursor.execute(sql,(id_usuario,id_producto))
+
+                row2=cursor.fetchone()
+
+                #Si hay resultados
+                if row2:
+
+                    #Actualizamos la cantidad y el precio de ese producto en la tabla
+                    # carrito asociado al usuario y al producto
+                    sql='UPDATE carrito SET cantidad=cantidad+1 and precio=precio+precio WHERE id_usuario=%s and id_producto=%s'
+                    cursor.execute(sql,(id_usuario,id_producto))
+                    db.connection.commit()
+
+                else:
+                
+                    #Sino, insertamos en la tabla carrito, el id del usuario,producto,cantidad del producto y precio total
+                    sql='INSERT INTO carrito (id_usuario,id_producto,cantidad,precio) VALUES (%s,%s,%s,%s)'
+                    cursor.execute(sql,(id_usuario,id_producto,1,precio))
+                    db.connection.commit()
 
                 #Quitamos 1 al stock del producto requerido
                 sql='UPDATE productos SET stock=stock-1 WHERE id=%s'
                 cursor.execute(sql,(id_producto,))
                 db.connection.commit()
 
-                #Devolvemos true para indicar que todo salio bien
+                #Devolvemos true para indicar que todo salió bien
                 return True
             
             #Sino, devolvemos None
@@ -45,6 +62,126 @@ class CartService:
             print(error)
             print('Producto no añadido')
             return None
+        
+    
+    @classmethod
+    def removeOneProductCart(cls,db,id_usuario,id_producto):
+        try:
+            #Se abre el cursor de la db
+            cursor=db.connection.cursor()
+
+            #Obtenemos el stock y el precio del producto requerido
+            sql='SELECT precio FROM productos WHERE id=%s'
+            cursor.execute(sql,(id_producto,))
+
+            row=cursor.fetchone()
+
+            #Si el producto existe
+            if row:
+
+                #Consultamos a ver si el producto ya fue añadido al carrito antes
+                sql='SELECT cantidad FROM carrito WHERE id_usuario=%s and id_producto=%s'
+                cursor.execute(sql,(id_usuario,id_producto))
+
+                row2=cursor.fetchone()
+
+                #Si el producto esta en carrito asociado al usuario
+                if row2:
+                    cantidad=row2[0]
+
+                    #Si la cantidad es una, borramos la fila de la tabla carrito
+                    if cantidad==1:
+                        sql='DELETE FROM carrito WHERE id_usuario=%s and id_producto=%s'
+                        cursor.execute(sql,(id_usuario,id_producto))
+                        db.connection.commit()
+
+                    else:
+
+                        #Sino restamos la cantidad y el precio de ese producto 
+                        # en la tabla carrito asociado al usuario y al producto
+                        sql='UPDATE carrito SET cantidad=cantidad-1 and precio=precio-precio WHERE id_usuario=%s and id_producto=%s'
+                        cursor.execute(sql,(id_usuario,id_producto))
+                        db.connection.commit()
+
+                    #Añadimos 1 al stock del producto requerido
+                    sql='UPDATE productos SET stock=stock+1 WHERE id=%s'
+                    cursor.execute(sql,(id_producto,))
+                    db.connection.commit()
+
+                    #Devolvemos True para indicar que todo salio bien
+                    return True
+
+                #Sino, devolvemos None          
+                else:
+                    return None
+                
+            #Sino, devolvemos None
+            else:
+                print('Producto erroneo')
+                return None
+
+        #Si hay errores, devolvemos None
+        except Exception as error:
+            print(error)
+            print('Producto no añadido')
+            return None
+        
+
+    @classmethod
+    def removeProductCart(cls,db,id_usuario,id_producto):
+        try:
+            #Se abre el cursor de la db
+            cursor=db.connection.cursor()
+
+            #Obtenemos el stock y el precio del producto requerido
+            sql='SELECT precio FROM productos WHERE id=%s'
+            cursor.execute(sql,(id_producto,))
+
+            row=cursor.fetchone()
+
+            #Si el producto existe
+            if row:
+
+                #Consultamos a ver si el producto ya fue añadido al carrito antes
+                sql='SELECT cantidad FROM carrito WHERE id_usuario=%s and id_producto=%s'
+                cursor.execute(sql,(id_usuario,id_producto))
+
+                row2=cursor.fetchone()
+
+                #Si el producto esta en carrito asociado al usuario
+                if row2:
+                    cantidad=row2[0]
+
+                    sql='DELETE FROM carrito WHERE id_usuario=%s and id_producto=%s'
+                    cursor.execute(sql,(id_usuario,id_producto))
+                    db.connection.commit()
+
+                    
+
+                    #Añadimos 1 al stock del producto requerido
+                    sql='UPDATE productos SET stock=stock+%s WHERE id=%s'
+                    cursor.execute(sql,(cantidad,id_producto))
+                    db.connection.commit()
+
+                    #Devolvemos True para indicar que todo salio bien
+                    return True
+
+                #Sino, devolvemos None          
+                else:
+                    return None
+                
+            #Sino, devolvemos None
+            else:
+                print('Producto erroneo')
+                return None
+
+        #Si hay errores, devolvemos None
+        except Exception as error:
+            print(error)
+            print('Producto no añadido')
+            return None
+        
+    
         
 
 
