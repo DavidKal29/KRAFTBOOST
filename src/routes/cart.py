2c64661004e25577b4119baed3338e8c6675d4ec
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-19 13:44:12 +0200

    Creamos una funcion que se encargue de mirar si estamos logueados y somos clientes, y luego validar eso en cada ruta con esa funcion y no manualmente todo el rato, haciendo el código más limpio y mas corto.

--- src/routes/cart.py
+++ src/routes/cart.py
@@ -2,76 +2,63 @@ from flask import Blueprint,abort,redirect,url_for,render_template,request,curre
 from flask_login import current_user
 from services.CartService import CartService
 
-
 cart_bp=Blueprint('cart',__name__,url_prefix='/cart')
 
-
-@cart_bp.route('/',methods=['GET','POST'])
-def cart():
-    if current_user.is_authenticated:
-        print('EL current user rol:',current_user.rol)
-        if current_user.rol=='client':
-            db=current_app.config['db']
-
-            productos_carrito=CartService.showAllProductsInCart(db,current_user.id)
-
-            subtotal=CartService.showSumario(db,current_user.id)
-
-            if subtotal:
-                subtotal=float(subtotal)
-            
-            return render_template('cart.html',productos_carrito=productos_carrito,subtotal=subtotal)
-        else:
-            abort(401)
+def client_required():
+    if not current_user.is_authenticated:
+        return redirect(url_for('auth.login'))
     
-    return redirect(url_for('auth.login'))
+    elif not current_user.rol=='client':
+        abort(401)
+
+    else:
+        return True
+
+
+@cart_bp.route('/',methods=['GET'])
+def cart():
+    check=client_required()
+    if check:
+        db=current_app.config['db']
+
+        productos_carrito=CartService.showAllProductsInCart(db,current_user.id)
+
+        subtotal=CartService.showSumario(db,current_user.id)
+
+        if subtotal:
+            subtotal=float(subtotal)
+                
+        return render_template('cart.html',productos_carrito=productos_carrito,subtotal=subtotal)
 
 
 @cart_bp.route('/add_product/<id_producto>',methods=['GET'])
 def add_product(id_producto):
-    if current_user.is_authenticated:
-        print('EL current user rol:',current_user.rol)
-        if current_user.rol=='client':
-            db=current_app.config['db']
-            
-            print('Aqui ira el codigo para añadir el producto')
+    check=client_required()
+    if check:
+        db=current_app.config['db']
 
-            CartService.addProductCart(db,current_user.id,id_producto)
+        CartService.addProductCart(db,current_user.id,id_producto)
             
-            return redirect(url_for('cart.cart'))
-        else:
-            abort(401)
-    
-    return redirect(url_for('auth.login'))
+        return redirect(url_for('cart.cart'))
 
 
 @cart_bp.route('/removeOneProduct/<id_producto>',methods=['GET'])
 def removeOneProduct(id_producto):
-    if current_user.is_authenticated:
-        print('EL current user rol:',current_user.rol)
-        if current_user.rol=='client':
-            db=current_app.config['db']
+    check=client_required()
+    if check:
+        db=current_app.config['db']
 
-            CartService.removeOneProductCart(db,current_user.id,id_producto)
+        CartService.removeOneProductCart(db,current_user.id,id_producto)
             
-            return redirect(url_for('cart.cart'))
-        else:
-            abort(401)
-    
-    return redirect(url_for('auth.login'))
+        return redirect(url_for('cart.cart'))
 
 
 @cart_bp.route('/removeProduct/<id_producto>',methods=['GET'])
 def removeProduct(id_producto):
-    if current_user.is_authenticated:
-        print('EL current user rol:',current_user.rol)
-        if current_user.rol=='client':
-            db=current_app.config['db']
+    check=client_required()
+    if check:
+        db=current_app.config['db']
 
-            CartService.removeProductCart(db,current_user.id,id_producto)
+        CartService.removeProductCart(db,current_user.id,id_producto)
             
-            return redirect(url_for('cart.cart'))
-        else:
-            abort(401)
-    
-    return redirect(url_for('auth.login'))
+        return redirect(url_for('cart.cart'))
