283b042a4d57ef3c4ff1967f82499b8c03a4b27f
DavidKal29 <davidkalmuk65@gmail.com>
2025-05-25 13:56:37 +0200

    Agregamos a la seccion de productos del admin, la paginacion, los filtros y demás, para agilizar la búsqueda de los productos.

--- src/models/ModelProduct.py
+++ src/models/ModelProduct.py
@@ -354,7 +354,7 @@ class ModelProduct():
         
     #Metodo para construir el where de la consulta para msotrar productos
     @classmethod
-    def construir_where(cls,parametros,categorias):
+    def construir_where(cls,parametros,categorias,admin=False):
         
         #Creamos la lista donde van las condiciones del where y 
         # la usaré para luego juntar todo con un join
@@ -365,11 +365,11 @@ class ModelProduct():
         if parametros:
 
             if 'marca' in parametros:
-                condiciones.append("id_marca={}".format(parametros['marca']))
+                condiciones.append("p.id_marca={}".format(parametros['marca']))
 
 
             if 'categoria' in parametros:
-                condiciones.append("id_categoria={}".format(parametros['categoria']))
+                condiciones.append("p.id_categoria={}".format(parametros['categoria']))
 
 
             if 'precio' in parametros:
@@ -379,7 +379,7 @@ class ModelProduct():
                 precio_min=rangos[0]
                 precio_max=rangos[1]
 
-                condiciones.append("precio>={} AND precio<={}".format(precio_min,precio_max))
+                condiciones.append("p.precio>={} AND p.precio<={}".format(precio_min,precio_max))
 
 
             #Si alguien buscó algo en el buscador
@@ -401,7 +401,7 @@ class ModelProduct():
                     #Si es un número, añade a las condiciones_numeros con el kg
                     if termino.isdigit():
                     
-                        condiciones_numeros.append("nombre LIKE '%% {}kg%%'".format(termino))
+                        condiciones_numeros.append("p.nombre LIKE '%% {}kg%%'".format(termino))
                     
 
                     #Si no es un numero
@@ -412,25 +412,25 @@ class ModelProduct():
                             termino=float(termino)
                                 
                             #Si es un decimal se añade como si fuera un numero mas, con el kg
-                            condiciones_numeros.append("nombre LIKE '%% {}kg%%'".format(termino))
+                            condiciones_numeros.append("p.nombre LIKE '%% {}kg%%'".format(termino))
         
                                 
                         #Si falla es porque es una palabra   
                         except Exception as error:
 
                             if termino=='kg':
-                                condiciones_kilos.append("nombre LIKE '%%{}%%'".format(termino))
+                                condiciones_kilos.append("p.nombre LIKE '%%{}%%'".format(termino))
 
                             #Si es alguna marca lo añade a las condiciones_marcas
                             elif termino in ['domyos','maniak','corength','e-series','kraftboost','tunturi']:
-                                condiciones_marcas.append("nombre LIKE '%%{}%%'".format(termino))
+                                condiciones_marcas.append("p.nombre LIKE '%%{}%%'".format(termino))
 
                             elif termino in ['inclinado','plano','abierto','cuerda','metal','neutro','unilateral','estrecho','gironda','hierro','goma','metalico']:
-                                condiciones_materiales.append("nombre LIKE '%%{}%%'".format(termino))
+                                condiciones_materiales.append("p.nombre LIKE '%%{}%%'".format(termino))
 
                             #Sino ya lo mete en condiciones_palabras    
                             else:
-                                condiciones_palabras.append("nombre LIKE '%%{}%%'".format(termino))
+                                condiciones_palabras.append("p.nombre LIKE '%%{}%%'".format(termino))
                 
                 
                 #Con cada lista, juntamos los strings con ors y dentro de parentesis, 
@@ -454,8 +454,12 @@ class ModelProduct():
             #si hay condiciones, mete un WHERE y luego, los 
             # strings separados por ands usando el join
             if condiciones:
-
-                condiciones=' WHERE activo=1 AND '+' AND '.join(condiciones)
+                
+                if not admin:
+                    condiciones=' WHERE p.activo=1 AND '+' AND '.join(condiciones)
+                else:
+                    condiciones=' WHERE '+' AND '.join(condiciones)
+                
                 print('Las condiciones ya procesadas:',condiciones)
                 
                 return condiciones
@@ -469,17 +473,21 @@ class ModelProduct():
     #Funcion para /shop para devolver el total de productos que se 
     # necesitará para hacer lo de la paginacion
     @classmethod
-    def mostrar_contador_productos(cls,db,parametros,categorias):
+    def mostrar_contador_productos(cls,db,parametros,categorias,admin=False):
         try:
             #Se abre un cursor con la conexion a la db
             cursor=db.connection.cursor()
         
-            #Creamos la consulta base
-            sql='SELECT COUNT(*) FROM productos WHERE activo=1'
-            
+            sql='SELECT COUNT(*) FROM productos p'
             #Añado al sql, las condiciones del where
-            condiciones=cls.construir_where(parametros, categorias)
+            if not admin:
+                condiciones=cls.construir_where(parametros,categorias)
+            else:
+                condiciones=cls.construir_where(parametros,categorias,admin=True)
+                
             sql+=condiciones
+
+            print('El sql final dsde mostrar contador:',sql)
             
             #Ejecutamos la consulta
             cursor.execute(sql)
@@ -510,7 +518,7 @@ class ModelProduct():
     #Funcion para /shop, donde se mostraran los productos y se filtraran con LIMIT y OFFSET
     # para que se muestren asi con la paginación
     @classmethod
-    def mostrar_productos_paginacion(cls,db,page,productos_por_pagina,orden,parametros,categorias):
+    def mostrar_productos_paginacion(cls,db,page,productos_por_pagina,orden,parametros,categorias,admin=False):
         try:
             #Se abre un cursor con la conexion a la db
             cursor=db.connection.cursor()
@@ -525,28 +533,42 @@ class ModelProduct():
             
 
             #Montamos la consulta base
-            sql='SELECT id,nombre,precio,imagen FROM productos'
+            if not admin:
+                sql='SELECT id,nombre,precio,imagen FROM productos p'
+            else:
+                sql='''
+                    SELECT p.id,p.nombre,p.stock,p.precio,p.activo,p.ventas,m.nombre,c.nombre FROM productos p 
+                    INNER JOIN marcas m
+                    ON p.id_marca=m.id
+                    INNER JOIN categorias c
+                    ON p.id_categoria=c.id
+                '''
+
 
             #Añado al sql, las condiciones del where
-            condiciones=cls.construir_where(parametros, categorias)
+            if not admin:
+                condiciones=cls.construir_where(parametros,categorias)
+            else:
+                condiciones=cls.construir_where(parametros,categorias,admin=True)
             sql+=condiciones
 
             if not condiciones:
-                sql+=' WHERE activo=1'
+                if not admin:
+                    sql+=' WHERE p.activo=1'
 
 
             #Mira el orden por cual filtrar
             if orden=='masRecientes':
-                sql+=' ORDER BY id DESC'
+                sql+=' ORDER BY p.id DESC'
 
             elif orden=='topVentas':
-                sql+=' ORDER BY ventas DESC'
+                sql+=' ORDER BY p.ventas DESC'
 
 
             #Añadimos al final el limit y el offset con /%s
             sql+=' LIMIT %s OFFSET %s'
 
-            print(sql)
+            print('El sql final desde mostrar productos:',sql)
 
             values=(limit,offset)
             
@@ -560,13 +582,30 @@ class ModelProduct():
             if resultados:
                 productos=[]
 
-                for resultado in resultados:
-                    id=resultado[0]
-                    nombre=resultado[1]
-                    precio=resultado[2]
-                    imagen=resultado[3]
+                if not admin:
+                    for resultado in resultados:
+                        
+                        id=resultado[0]
+                        nombre=resultado[1]
+                        precio=resultado[2]
+                        imagen=resultado[3]
 
-                    productos.append(Product(id,nombre, precio, None, None, None, imagen))
+                        productos.append(Product(id,nombre, precio, None, None, None, imagen))
+                
+                else:
+                    print('Estamos ante el admin, obteniendo todito')
+                    for resultado in resultados:
+                        id=resultado[0]
+                        nombre=resultado[1]
+                        stock=resultado[2]
+                        precio=resultado[3]
+                        activo=resultado[4]
+                        ventas=resultado[5]
+                        marca=resultado[6]
+                        categoria=resultado[7]
+                    
+
+                        productos.append(Product(id,nombre,precio,marca,categoria,0,0,stock,ventas,activo))
 
                 cursor.close()
                 return productos
--- src/routes/admin.py
+++ src/routes/admin.py
@@ -8,6 +8,8 @@ from models.ModelProduct import ModelProduct
 from models.ModelBrand import ModelBrand
 from models.ModelCategory import ModelCategory
 from models.entities.Product import Product
+import unidecode
+import math
 
 
 admin_bp=Blueprint('admin',__name__,url_prefix='/admin')
@@ -240,7 +242,6 @@ def delete_user(id):
 
 
 
-
 @admin_bp.route('/orders',methods=['GET'])
 def orders():
     try:
@@ -333,7 +334,7 @@ def delete_order(id):
 
 
 
-@admin_bp.route('/products',methods=['GET'])
+@admin_bp.route('/products',methods=['GET','POST'])
 def products():
     try:
         check=admin_required()
@@ -341,15 +342,207 @@ def products():
             return check
         
         else:    
-            #Obtenemos el cursor de la db
-            db=current_app.config['db']
+            if request.method=='GET':
+                #Variable que mirará si alguien puso algo raro en los filtros desde las rutas
+                bugs=False
+                
+                #Marcamos los parámetros validos
+                parametros_validos=['page','marca','categoria','precio','orden','search']
+                
+                #Obtenemos el db de la app
+                db=current_app.config['db']
 
-            #Obtenemos los productos
-            productos=ModelProduct.getAllProducts(db)
+                #Obtenemos las marcas, categorias y precios de app
+                marcas=current_app.config['marcas']
+                categorias=current_app.config['categorias']
+                precios=current_app.config['precios']
 
-    
-            return render_template('admin/products.html',productos=productos)            
-           
+
+                #Obtenemos los parametros de filtrado
+                orden=request.args.get('orden')
+                search=request.args.get('search')
+                marca=request.args.get('marca')
+                categoria=request.args.get('categoria')
+                precio=request.args.get('precio')
+
+
+                #Creamos un diccionario y vemos si estos valores existen en la url
+                parametros={}
+                
+
+                #Si el search existe y no es vacio,lo añadimos y au
+                if search=='':
+                    bugs=True
+                    search=None
+
+                elif search:
+                    search=search.strip()
+                    search=search.lower()
+                    search=' '.join(search.split())
+                    search=unidecode.unidecode(search)
+                    parametros['search']=search
+                        
+                
+
+
+                #Si la marca existe, es un numero y esta en el rango de len(marcas), 
+                # se añade a parametros, sino se activa a True los bugs
+                if marca:
+                    if not marca.isdigit():
+                        bugs=True
+                    
+                    elif int(marca)>len(marcas) or int(marca)<1:
+                        bugs=True
+                    
+                    else:
+                        parametros['marca']=int(marca)
+                
+                #Si la categoria existe, es un numero y esta en el rango de len(categorias), 
+                # se añade a parametros, sino se activa a True los bugs
+                if categoria:
+                    if not categoria.isdigit():
+                        bugs=True
+                    
+                    elif int(categoria)>len(categorias) or int(categoria)<1:
+                        bugs=True
+                    
+                    else:
+                        parametros['categoria']=int(categoria)
+
+                #Si precio existe y esta en los rangos correctos se añade a paramtros sino bugs True
+                if precio:
+                    if precio in precios:
+                        parametros['precio']=precio
+                    else:
+                        bugs=True
+
+                
+                #Miramos si no hay parametros que no deberian estar o hay parametros duplicados 
+                for i in request.args.keys():
+                    if i not in parametros_validos or len(request.args.getlist(i))>1:
+                        bugs=True
+                
+
+                #Si el orden no existe, es un numero, o tiene un valor invalido, filtramos por los mas recientes
+                if not orden or not orden.isalpha() or orden not in ['masRecientes','topVentas']:
+                    orden='masRecientes'
+                    bugs=True
+
+                
+                #Si no hay page, redirijimos a page 1
+                if not request.args.get('page'):
+                    bugs=True
+                    page=1
+                
+                
+                #Definimos el numero de paginas
+                productos_por_pagina=12
+
+                #Intento obtener el page en forma de int
+                try:
+                    page=int(request.args['page'])
+
+                #Caerá aquí si el page contiene letras
+                except Exception as error:
+                    print(error)
+                    page=1
+                    bugs=True
+
+                #Comprobamos por primera vez si no hay bugs
+                if bugs:
+                    return redirect(url_for('admin.products',page=page,orden=orden,**parametros))
+                
+                
+                #Obtenemos el numero total de productos segun los parametros
+                total=ModelProduct.mostrar_contador_productos(db,parametros,categorias,admin=True)
+                
+                
+                #Si el numero total es un numero establecemos la pagina 
+                # maxima a ese numero entre los productos por pagina 
+                # redondeando al mayor por si da 1.5 o cosas asi, sino 1
+                if total:
+                    pagina_maxima=math.ceil(total/productos_por_pagina)
+                else:
+                    pagina_maxima=1
+
+                    
+                #Si el page es mayor a la pagina maxima, redirije a la pagina maxima
+                if page>pagina_maxima:
+                    page=pagina_maxima
+                    bugs=True
+                    
+                #Si es menor a 1, lo lleva a 1
+                elif page<1:
+                    page=1
+                    bugs=True
+                    
+
+                #Comprobamos por segunda vez si hay bugs
+                if bugs:
+                    return redirect(url_for('admin.products',page=page,orden=orden,**parametros))
+
+                #Si porfin todo sale bien
+                else:
+                    #Obtenemos los productos con los filtros de la paginacion
+                    productos=ModelProduct.mostrar_productos_paginacion(db,page,productos_por_pagina,orden,parametros,categorias,admin=True)
+                
+
+                return render_template('admin/products.html',
+                                        paginas=pagina_maxima,productos=productos,
+                                        page=page, parametros=parametros,
+                                        marcas=marcas,categorias=categorias,precios=precios,orden=orden)
+                
+            
+            
+            elif request.method=='POST':
+                #Lo mismo, creamos un diccionario y vemos si los parametros han sido seleccionados
+                parametros={}
+                
+                #Obtenemos el search del formulario
+                search=request.form.get('search')
+
+
+                #Si está la metemos en los parametros
+                if not  search:
+                    search=request.args.get('search')
+
+                if search:
+                    #Quitar espacios, hacer todo en minusculas y quitar tildes
+                    search=search.strip()
+                    search=search.lower()
+                    search=' '.join(search.split())
+                    search=unidecode.unidecode(search)
+                    
+                    parametros['search']=search
+
+                    
+
+                #Obtenemos los parametros de los select
+                orden=request.form.get('select_orden')
+                marca=request.form.get('select_marca')
+                categoria=request.form.get('select_categoria')
+                precio=request.form.get('select_precio')
+
+                
+                #Obtenemos el page
+                page=request.args.get('page')
+
+                
+                #Validamos que no sean values 0
+                if marca and marca!='0':
+                    parametros['marca']=marca
+                
+                if categoria and categoria!='0':
+                    parametros['categoria']=categoria
+
+                if precio and precio!='0':
+                    parametros['precio']=precio
+
+                print('Los parametrillos:',parametros)
+
+                #Redirijimos con los parametros
+                return redirect(url_for('admin.products',page=page,orden=orden,**parametros))        
+            
 
     #Cualquier error nos lleva a 404
     except Exception as error:
@@ -375,7 +568,7 @@ def deactivate_product(id):
             #Si el producto fue desactivado
             if desactivado:
                 #Mandamos a los productos
-                return redirect(url_for('admin.products'))
+                return redirect(request.referrer or url_for('admin.products'))
             
             #Sino, mandamos al 404
             else:
@@ -406,7 +599,7 @@ def activate_product(id):
             #Si el producto fue activado
             if activado:
                 #Mandamos a los productos
-                return redirect(url_for('admin.products'))
+                return redirect(request.referrer or url_for('admin.products'))
             
             #Sino, mandamos al 404
             else:
